54cb478dc15187627126b8cd15f5000d4543011a
saguaroStickup <mindlessjockabird@gmail.com>
2018-10-24 20:48:38 -0500

    deleting from slack

--- server.js
+++ server.js
@@ -18,19 +18,61 @@ server.route({
 	method:'GET',
   path:'/hello',
 	handler: async function(request,h){
-		const params = {
-			token: process.env.SLACK_TOKEN,
-			types: 'images,pdfs,zips,',
-		}
 		try {
-			const response = await bot.files.list(params);
-			console.log('RESPONSE: ', response); 
+			const response = await getFiles();
+			if (response.ok) {
+				console.log('LENGTH: ', response.files.length);
+				try {
+					await deleteFiles(response.files);
+				} catch (e) {
+					console.log('Error deleting file: ', e);
+				}
+				return 'ok';	
+			}
  		} catch(e) {
 			console.log(e);
 		}
 	}
 });
 
+async function deleteFiles(files){
+	files.forEach((file, idx) => {
+		try {
+			const params = {
+				file: file.id,
+				token: process.env.SLACK_TOKEN
+			}
+			if (idx % 50 == 0) {
+			//	setTimeout(() => deleteFile(params), 60000);
+			} else {
+				deleteFile(params);
+			}
+		} catch(e) {
+			console.log('Unable To Delete File: ', e);	
+		}
+	});
+}
+
+async function deleteFile(params) {
+	try {
+		const file = await bot.files.delete(params);
+		console.log('FILE: ', file);
+		return 'ok';
+	} catch(e) {
+		console.log('Error deleting file: ', e);
+	}
+}
+
+async function getFiles() {
+	let date = new Date();
+	date.setDate(date.getDate() - 30);
+	const params = {
+		token: process.env.SLACK_TOKEN,
+		types: 'images,pdfs,zips',
+		ts_to: date,
+	}
+	return await bot.files.list(params);
+}
 
 async function start() {
 	try { 
